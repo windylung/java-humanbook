87854feec9c2bfee61cc88df3564f8b1676e6a79
windylung <jisoolee031@gmail.com>
2024-04-20 23:02:59 +0900

    feat: Spring Boot Project 생성

---
+++ src/main/java/project/humanbook/humanbook/HumanbookApplication.java
@@ -0,0 +1,13 @@
+package project.humanbook.humanbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/humanbook/humanbook/HumanbookApplicationTests.java
@@ -0,0 +1,13 @@
+package project.humanbook.humanbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
