ae98adcd2d8198a0bfe6af88b0b23cf573fe132b
windylung <jisoolee031@gmail.com>
2024-05-27 15:21:59 +0900

    feat. aws DB 연결/Book Table 연결

--- src/main/java/project/humanbook/humanbook/BookController.java
+++ src/main/java/project/humanbook/humanbook/BookController.java
@@ -1,5 +1,6 @@
 package project.humanbook.humanbook;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,17 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
+
 @Controller
 public class BookController {
-    private final BookService bookService = new BookService();
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
 
     @GetMapping("/")
     public String getMethodName(Model model) {
@@ -18,20 +27,22 @@ public class BookController {
     }
 
     @PostMapping("/save")
-    public String postMethodName(@ModelAttribute Book book, Model model) {
-        bookService.addBook(book.getTitle(), book.getAuthor());
-        // model.addAttribute("books", bookService.getAllBooks());
-        // System.out.println(model);
-        // return "main";
+    public String saveBook(@ModelAttribute Book book) {
+        bookService.saveBook(book);
         return "redirect:/";
     }
 
     @GetMapping("/book-detail")
     public String getBookDetail(@RequestParam("id") Integer bookId, Model model) {
-        Book book = bookService.findById(bookId);
-        model.addAttribute("title", book.getTitle());
-        model.addAttribute("author", book.getAuthor());
-        return "bookDetail";
+        Optional<Book> bookOptional = bookService.findById(bookId);
+        if (bookOptional.isPresent()) {
+            Book book = bookOptional.get();
+            model.addAttribute("title", book.getTitle());
+            model.addAttribute("author", book.getAuthor());
+            return "bookDetail";
+        } else {
+            model.addAttribute("error", "No book found with ID " + bookId);
+            return "redirect:/";
+        }
     }
-
 }
--- src/main/java/project/humanbook/humanbook/BookService.java
+++ src/main/java/project/humanbook/humanbook/BookService.java
@@ -1,32 +1,28 @@
 package project.humanbook.humanbook;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
-  private List<Book> books = new ArrayList<>();
+  private final BookRepository bookRepository;
 
-  public BookService() {
-    books.add(new Book(0, "세상의 모든 건강한 변화를 사랑합니다", "작가1"));
-    books.add(new Book(1, "우당탕탕 서울 전세집 구하기", "작가2"));
-    books.add(new Book(2, "How to Dance with Rhythm", "작가3"));
-    books.add(new Book(3, "카세트테이프는 언제나 따뜻했다", "작가4"));
-    books.add(new Book(4, "복에 겨운 소리", "작가5"));
+  @Autowired
+  public BookService(BookRepository bookRepository){
+    this.bookRepository = bookRepository;
+  }
+  public Book saveBook(Book book) {
+    return bookRepository.save(book);
   }
 
   public List<Book> getAllBooks() {
-    return books;
+    return bookRepository.findAll();
   }
 
-  public void addBook(String title, String author) {
-    Integer len = books.size();
-    books.add(new Book(len, title, author));
-  }
-
-  public Book findById(Integer id) {
-    Book book = books.get(id);
-    return book;
+  public Optional<Book> findById(Integer id){
+    return bookRepository.findById(id);
   }
 }
