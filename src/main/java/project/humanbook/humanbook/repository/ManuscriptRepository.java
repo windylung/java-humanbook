14a21c7b9336a28201d1d743db299a0789108488
windylung <jisoolee031@gmail.com>
2024-06-09 23:07:45 +0900

    feat. 질문

--- src/main/java/project/humanbook/humanbook/repository/ManuscriptRepository.java
+++ src/main/java/project/humanbook/humanbook/repository/ManuscriptRepository.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Repository;
 import project.humanbook.humanbook.entity.Manuscript;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ManuscriptRepository extends JpaRepository<Manuscript, Long> {
     List<Manuscript> findAllByUserId(Long userId);
+    Optional<Manuscript> findByUserIdAndStep(Long userId, int step);
 }
--- src/main/java/project/humanbook/humanbook/service/ManuscriptService.java
+++ src/main/java/project/humanbook/humanbook/service/ManuscriptService.java
@@ -6,6 +6,7 @@ import project.humanbook.humanbook.repository.ManuscriptRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ManuscriptService {
@@ -28,4 +29,26 @@ public class ManuscriptService {
         return manuscriptRepository.findAllByUserId(userId);
     }
 
+    public Optional<Manuscript> getManuscriptByUserIdAndStep(Long userId, int step) {
+        return manuscriptRepository.findByUserIdAndStep(userId, step);
+    }
+
+    public Manuscript updateManuscript(Long userId, int step, String title, String content, LocalDateTime localDateTime) {
+        Optional<Manuscript> optionalManuscript = manuscriptRepository.findByUserIdAndStep(userId, step);
+        if (optionalManuscript.isPresent()) {
+            Manuscript manuscript = optionalManuscript.get();
+            manuscript.setTitle(title);
+            manuscript.setContent(content);
+            manuscript.setLastModified(localDateTime);
+            return manuscriptRepository.save(manuscript);
+        } else {
+            Manuscript manuscript = new Manuscript();
+            manuscript.setUserId(userId);
+            manuscript.setStep(step);
+            manuscript.setTitle(title);
+            manuscript.setContent(content);
+            manuscript.setLastModified(localDateTime);
+            return manuscriptRepository.save(manuscript);
+        }
+    }
 }
